25b1dc4e63e5298202cab668bc97ea982ce0d5df
AldwynForTheWin <elephunkies101@gmail.com>
2014-04-28 21:23:13 +0800

    enabled remove of philosophers from the list without affecting the forks; partial completion -- polishing and logging needed

--- Cabarrubias_DiningPhilosophers.py
+++ Cabarrubias_DiningPhilosophers.py
@@ -3,104 +3,93 @@ from threading import Thread, Lock
 import random
 import time
 
+class Fork(Node):
+	
+	def __init__(self, data):
+		Node.__init__(self, data)
+		self.lock = Lock()
+
+	def __str__(self):
+		return '<Fork: %s>' % self.data
+
+
 class Philosopher(Node, Thread):
 
 	def __init__(self, data, fork):
 		Node.__init__(self, data)
 		Thread.__init__(self)
 		self.right_fork = fork
-		self.c_list = None
+		self.philosophers = None
+		self.fork_list = None
 	
 	def __str__(self):
 		return '<Philosopher: %s>' % self.data
 	
-	def eat(self, delay):
+	def __eat(self, delay):
 		print '%s now eating for %d seconds...' % (self, delay)
 		time.sleep(delay)
 
-	def is_rfork_on(self):
-		print 'Attempting to grab right fork of %s' % self,
-		result = self.right_fork.locked
-		print 'Successful' if result else 'Failed'
-		return result
+	def __remove_self_from_philosophers(self):
+		self.philosophers.remove(self.data)
+		print '%s already left the table.' % self
 
-	def is_lfork_on(self):
-		print 'Attempting to grab left fork of %s' % self,
-		result = self.next.right_fork.locked
-		print 'Successful' if result else 'Failed'
-		return result
+	def set_fork_list(self, fork_list):
+		self.fork_list = fork_list
 
-	def is_forks_down(self):
-		pass
-
-	def set_c_list(self, c_list):
-		self.c_list = c_list
-
-	def remove_self_from_list(self):
-		self.c_list.remove(self.data)
-		print '%s already left the table' % self
+	def set_philosophers(self, philosophers):
+		self.philosophers = philosophers
 
 	def run(self):
-		if self.is_lfork_on() and self.is_rfork_on():
-			self.right_fork.acquire()
-			self.next.right_fork.acquire()
+		print '%s is already thinking.' % self
+		right_fork = self.fork_list.find(self.right_fork.data)
+		left_fork = self.fork_list.find(self.next.right_fork.data)
 
+		with left_fork.lock, right_fork.lock:
+			print '%s is using %s (L) and %s (R).' % (self, left_fork, right_fork)
 			delay = random.randint(1, 5)
-			self.eat(delay)
-			self.remove_self_from_list()
-			
-			self.right_fork.release()
-			self.next.right_fork.release()
-
+			self.__eat(delay)
+			self.__remove_self_from_philosophers()
 
 
 class DiningPhilosophers():
 	
 	def __init__(self):
 		self.philosophers = CircularLinkedList()
-		self.forks = CircularLinkedList()
+		self.fork_list = CircularLinkedList()
 
 		for i in xrange(1, 6):
-			fork = Lock()
-			self.forks.add(fork)
+			fork = Fork(i)
+			self.fork_list.add(fork)
 			self.philosophers.add(Philosopher(i, fork))
-			self.philosophers.find(i).set_c_list(self.philosophers)
+			self.philosophers.find(i).set_fork_list(self.fork_list)
+			self.philosophers.find(i).set_philosophers(self.philosophers)
 
 
-	def process(self):
-		self.philosophers.first.start()
-		initial = self.philosophers.first.next
-		while initial and initial != self.philosophers.first:
-			try:
-				initial.start()
-				initial = initial.next
-			except AttributeError:
-				pass
+	def run_threads(self):
+		initial, ctr = self.philosophers.first, self.philosophers.length
+		while ctr > 0:
+			initial.start()
+			initial = initial.next
+			ctr -= 1
 
-        # initial = squad.first
-        # while self.philosophers.length > 0:
-        #     if ctr % k == 0:
-        #         squad.remove(initial.data)
-        #     initial = initial.next
-        #     ctr += 1
 
-		self.philosophers.first.join()
-		initial = self.philosophers.first.next
-		while initial and initial != self.philosophers.first:
-			try:
-				initial.join()
-				initial = initial.next
-			except AttributeError:
-				pass
+		initial, ctr = self.philosophers.first, self.philosophers.length
+		while ctr > 0:
+			initial.join()
+			initial = initial.next
+			ctr -= 1
 
 		print self.philosophers
+
+		# self.philosophers.first.join()
+		# initial = self.philosophers.first.next
+		# while initial != self.philosophers.first:
+		# 	initial.join()
+		# 	initial = initial.next
 
 		
 
 if __name__ == '__main__':
 
 	dp = DiningPhilosophers()
-
-	print dp.philosophers
-	dp.process()
-	
+	dp.run_threads()
